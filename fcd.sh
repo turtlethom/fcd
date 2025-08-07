4d909d10f407fbd8c3087a9fe34b98adc8b8e0f1
Thomas James <wjamesthomas3@gmail.com>
2025-08-07 13:35:24 -0400

    1: Add fcd.sh, dev/pseudo.txt

---
+++ fcd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#############################################################################
+#<< ***** --- ( FCD ) --- ***** >># 
+# ->* Stands for 'fast/fuzzy change directory'
+# ->* Command that utilizes fzf for quickly changing into bookmarked directories
+# ->* Security-hardened fzf directory navigator with shortcuts & autocomplete.
+# ***** --- REQUIREMENTS --- ***** #
+# ->* fzf (for interactive selection)
+#############################################################################
+
+fcd() {
+  local BOOKMARKED = 
+  declare -a ORDERED_LABELS
+  declare -A PATHS
+  local SELECTED_KEY
+}
