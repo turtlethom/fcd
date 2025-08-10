d71258405914bfdf0e098a71039a7bc33baf3343
Thomas James <wjamesthomas3@gmail.com>
2025-08-10 17:18:22 -0400

    6: Rework logic in fcd and fcd_add, rename fcd.sh to fcd

--- lib/fcd_add.sh
+++ lib/fcd_add.sh
@@ -3,14 +3,9 @@
 fcd_add() {
   local fcd_file="$1"
   local lp_input="$2"
-  local lp_label="${lp_input%%=*}"
-  local lp_path="${lp_input#*=}"
-
-  # Check if path is a part of $HOME, is a directory
-  if [[ ! "$lp_path" == $HOME/* || ! -d "$lp_path" ]]; then
-    echo "Error: Argument must match 'LABEL=$HOME/...'"
-    return 1
-  fi
+  local lp_label="${lp_input%%:*}"
+  local lp_path="${lp_input#*:}"
+  local lp_adj_path="${lp_path/#\~/$HOME}"
 
   # Check if label has more than 10 characters
   if [[ "${#lp_label}" -gt 10 ]]; then
@@ -18,18 +13,28 @@ fcd_add() {
     return 1
   fi
 
+  if [[ "$lp_adj_path" != "$HOME"/* ]]; then
+    echo "Error: Path must be inside $HOME"
+    return 1
+  fi
+
+  if [[ ! -d "$lp_adj_path" ]]; then
+    echo "Error: '$lp_adj_path' is not a directory"
+    return 1
+  fi
+
   # Read file to check if path exists
   local found
-  found=$(read_and_check_file "$fcd_file" "$lp_label" "$lp_path")
-  write_bookmark "$fcd_file" "$lp_input" "$found"
+  found=$(read_and_check_file "$fcd_file" "$lp_label" "$lp_adj_path")
+  write_bookmark "$fcd_file" "$lp_label:$lp_adj_path" "$found"
 }
 
 read_and_check_file() {
   local fcd_file="$1"
-  local lp_path="$2"
-  local lp_label="$3"
+  local lp_label="$2"
+  local lp_adj_path="$3"
 
-  if grep -qE "^$lp_label=" "$fcd_file" || grep -qE "=$lp_label\$" "$fcd_file"; then
+  if grep -qE "^$lp_label:" "$fcd_file" || grep -qE ":$lp_adj_path\$" "$fcd_file"; then
     echo true
   else
     echo false
