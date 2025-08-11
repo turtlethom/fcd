a2cc2014cd55c3a8b3bbe387a4c3378770cb97fe
Thomas James <wjamesthomas3@gmail.com>
2025-08-11 01:25:18 -0400

    7: Rework fcd_add, move util commands to utils.sh

--- lib/fcd_add.sh
+++ lib/fcd_add.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source "$(dirname "${BASH_SOURCE[0]}")/utils.sh"
+
 fcd_add() {
   local fcd_file="$1"
   local lp_input="$2"
@@ -9,47 +11,23 @@ fcd_add() {
 
   # Check if label has more than 10 characters
   if [[ "${#lp_label}" -gt 10 ]]; then
-    echo "Error: Labels must be 10 characters or less"
+    echo "Error: Max character length of label exceeded"
     return 1
   fi
-
+  # Check if path is within $HOME
   if [[ "$lp_adj_path" != "$HOME"/* ]]; then
     echo "Error: Path must be inside $HOME"
     return 1
   fi
-
+  # Check if path is a directory
   if [[ ! -d "$lp_adj_path" ]]; then
     echo "Error: '$lp_adj_path' is not a directory"
     return 1
   fi
-
-  # Read file to check if path exists
-  local found
-  found=$(read_and_check_file "$fcd_file" "$lp_label" "$lp_adj_path")
-  write_bookmark "$fcd_file" "$lp_label:$lp_adj_path" "$found"
-}
-
-read_and_check_file() {
-  local fcd_file="$1"
-  local lp_label="$2"
-  local lp_adj_path="$3"
-
-  if grep -qE "^$lp_label:" "$fcd_file" || grep -qE ":$lp_adj_path\$" "$fcd_file"; then
-    echo true
+  # Handle finding label or path in file, write bookmark conditionally
+  if find_label_or_path "$fcd_file" "$lp_label" "$lp_adj_path"; then
+    write_bookmark "$fcd_file" "$lp_label:$lp_adj_path" 0
   else
-    echo false
-  fi
-}
-
-write_bookmark() {
-  local fcd_file="$1"
-  local lp_input="$2"
-  local found="$3"
-
-  if [[ "$found" == false ]]; then
-    echo "$lp_input" >> "$fcd_file"
-    echo "Bookmarked '$lp_input'."
-  else
-    echo "'$lp_input' already bookmarked."
+    write_bookmark "$fcd_file" "$lp_label:$lp_adj_path" 1
   fi
 }
--- lib/utils.sh
+++ lib/utils.sh
@@ -11,7 +11,7 @@ check_flag_used() {
   return 0
 }
 
-# <--- CHECK IF ARGUMENT FOR FLAG EXISTS (NOT ANOTHER FLAG)
+# <--- Check user args contain only one flag --> #
 check_flag_arg() {
   local arg="$1"
   local flag="$2"
@@ -20,4 +20,39 @@ check_flag_arg() {
     return 1
   fi
   return 0
+}
+
+# <--- Check if bookmark label exists ---> #
+find_bookmark_label() {
+  local fcd_file="$1"
+  local lp_label="$2"
+  grep -qE "^$lp_label:" "$fcd_file";
+}
+
+# <-- Check if bookmark path exists ---> #
+find_bookmark_path() {
+  local fcd_file="$1"
+  local path="$2"
+  grep -qE ":$path\$" "$fcd_file";
+}
+
+# <--- Find bookmark's label or path in file 'LABEL:$HOME/...' --> #
+find_label_or_path() {
+  local fcd_file="$1"
+  local lp_label="$2"
+  local lp_path="$3"
+  find_bookmark_label "$fcd_file" "$lp_label"|| find_bookmark_path "$fcd_file" "$lp_path"
+}
+
+write_bookmark() {
+  local fcd_file="$1"
+  local lp_input="$2"
+  local found="$3"
+
+  if (( found != 0 )); then
+    echo "$lp_input" >> "$fcd_file"
+    echo "Bookmarked '$lp_input'."
+  else
+    echo "'$lp_input' already bookmarked."
+  fi
 }
