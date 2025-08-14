20443f5da678dc45e703aadade7179c9c1f92003
Thomas James <wjamesthomas3@gmail.com>
2025-08-13 20:15:52 -0400

    18: Rework fcd_add to accept label:path & path

--- lib/fcd_add.sh
+++ lib/fcd_add.sh
@@ -5,10 +5,23 @@ source "$(dirname "${BASH_SOURCE[0]}")/utils.sh"
 __fcd_add() {
   local fcd_file="$1"
   local lp_input="$2"
-  local lp_label="${lp_input%%:*}"
+  local lp_label
+  local lp_path
+  local lp_adj_path
+
+  # <--- CHECK IF INPUT "~/some/path" OR "LABEL:~/some/path" ---> #
+  if [[ "$lp_input" == *:* ]]; then
+    lp_label="${lp_input%%:*}"
+    lp_path="${lp_input#*:}"
+  else
+    lp_path="$lp_input"
+    lp_label="$(basename "$lp_input")"
+  fi
+
   lp_label="${lp_label^^}"
-  local lp_path="${lp_input#*:}"
-  local lp_adj_path="${lp_path/#\~/$HOME}"
+  lp_adj_path="${lp_path/#\~/$HOME}"
+  # Strip '/' from directory if user includes it
+  lp_adj_path="${lp_adj_path#%/}"
 
   # Check if label has more than 25 characters
   if [[ "${#lp_label}" -gt 25 ]]; then
