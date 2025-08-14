7b5c3344857f1c7ffbcc0873b30d576aeafebf21
Thomas James <wjamesthomas3@gmail.com>
2025-08-13 20:16:42 -0400

    19: Add error check for fcd_remove

--- lib/fcd_remove.sh
+++ lib/fcd_remove.sh
@@ -4,6 +4,11 @@ __fcd_remove() {
   local file="$1"
   local label="$2"
   label="${label^^}"
+
+  if ! __find_bookmark_label "$file" "$label"; then
+    echo "Error: $label cannot be found" >&2
+    return 1
+  fi
   sed -i "/$label/d" $file
   echo "fcd: '$label' successfully removed"
 }
